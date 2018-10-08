3a974a56084bc340cb4562bbb3df051e8784aa3a
jo-qzy <2651933495@qq.com>
2018-10-08 21:00:36 +0800

    tcp服务端简单实现

--- tcp_server.c
+++ tcp_server.c
@@ -9,12 +9,14 @@
 
 int main(int argc, char* argv[])
 {
+	//1.创建套接字
     int sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (sockfd < 0)
     {
         perror("socket failed");
         return -1;
     }
+	//2.绑定地址
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(atoi(argv[2]));
@@ -26,6 +28,7 @@ int main(int argc, char* argv[])
         perror("bind failed");
         return -1;
     }
+	//3.开始监听（listen）
     ret = listen(sockfd, 5);
     if (ret < 0)
     {
@@ -38,6 +41,7 @@ int main(int argc, char* argv[])
         int new_sockfd;
         struct sockaddr_in client_addr;
         socklen_t client_addr_len = sizeof(client_addr);
+		//4.连接成功后获取新的套接字文件描述符
         new_sockfd = accept(sockfd, (struct sockaddr*)&client_addr, &client_addr_len);
         if (new_sockfd < 0)
         {
@@ -47,6 +51,7 @@ int main(int argc, char* argv[])
         while (1)
         {
             memset(buf, 0, 1024);
+			//5.接收数据
             ssize_t recv_len = recv(new_sockfd, buf, 1023, 0);
             if (recv_len < 0)
             {
@@ -64,11 +69,13 @@ int main(int argc, char* argv[])
                 printf("client[ip:%s, prot:%d]\n%s\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), buf);
                 memset(buf, 0, 1024);
                 scanf("%s", buf);
+				//6.发送数据
                 send(new_sockfd, buf, strlen(buf), 0);
             }
         }
         close(new_sockfd);
     }
+	//7.关闭套接字
     close(sockfd);
     return 0;
 }
